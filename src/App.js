ae16b4b9a325a8b363b196fe4dde30626aba255a
Alex V <alej.alvarado94@gmail.com>
2021-03-31 23:22:14 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Blog from '@xandrev/gistblog'
+import '@xandrev/gistblog/dist/index.css'
+
+const App = () => {
+  return <Blog user="seisvelas" />
+}
+
+export default App
